83b82dcc7741b4d5eae61e5e053c1fe4c411f33a
Chad <dev.chadlim@gmail.com>
2017-04-19 20:38:11 +0900

    LoginActivity
    server<->client login using session key

--- app/src/main/java/kr/co/appcode/teamcloud/LoginActivity.java
+++ app/src/main/java/kr/co/appcode/teamcloud/LoginActivity.java
@@ -4,10 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,10 +35,15 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editId;
     private EditText editPassword;
 
+    private CallbackManager callbackManager;
+    private String cookie = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
 
         layoutEditId = (TextInputLayout) findViewById(R.id.layout_editId);
         layoutEditPassword = (TextInputLayout) findViewById(R.id.layout_editPassword);
@@ -47,7 +71,21 @@ public class LoginActivity extends AppCompatActivity {
                 } else if(password.contains(" ")){
                     layoutEditPassword.setError("비밀번호에 공백은 허용하지 않습니다.");
                 }else {
-                    Toast.makeText(LoginActivity.this, "Sign in", Toast.LENGTH_SHORT).show();
+                    HashMap<String, String> values = new HashMap<>();
+                    values.put("id", id);
+                    values.put("password", password);
+                    try {
+                        HttpPostThread httpPostThread = new HttpPostThread(values, new URL("http://appcode.co.kr/TeamCloud/login.php"), 1);
+                        httpPostThread.start();
+                        httpPostThread.join();
+                        Log.d("return", ""+httpPostThread.getResultCode());
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra("cookie", cookie);
+                        startActivity(intent);
+                    } catch (MalformedURLException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -60,5 +98,86 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.btn_facebook_login);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                final GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback(){
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v("result", object.toString());
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("LoginError", error.toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class HttpPostThread extends Thread {
+        private HashMap<String, String> values;
+        private URL url;
+        private int resultCode;
+        private int mode;
+
+        private HttpPostThread(HashMap<String, String> values, URL url, int mode) {
+            this.values = values;
+            this.url = url;
+            this.mode = mode;
+        }
+
+        public void run() {
+            try {
+                String body = "id="+values.get("id")+"&password="+values.get("password");
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+                OutputStream os = conn.getOutputStream();
+                os.write(body.getBytes("UTF-8"));
+                os.flush();
+                os.close();
+
+                cookie = conn.getHeaderField("Set-Cookie");
+                if(cookie != null){
+                    Log.d("cookie", cookie);
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                resultCode = Integer.valueOf(br.readLine());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private int getResultCode() {
+            return resultCode;
+        }
     }
 }
